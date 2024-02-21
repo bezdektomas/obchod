989bdaec04f12ec954f7e167a6aef2a8492ee603
Tomáš Bezděk <tb.bezdek@gmail.com>
2024-02-21 11:31:36 +0100

    end

--- obchod/src/main/java/fim/obchod/Obchod.java
+++ obchod/src/main/java/fim/obchod/Obchod.java
@@ -1,8 +1,16 @@
 package fim.obchod;
 
+import java.util.List;
+
 public interface Obchod {
     public void pridejDoNabidky(Produkt produkt);
     public void odstranZNabidky(Produkt produkt);
 
     public boolean jeVNabidce(Produkt produkt);
+    public void ulozNabidku(String fileName);
+    public void nactiNabidku(String fileName);
+    public void vymazNabidku();
+
+    public List<Produkt> nabidka();
+
 }
---
+++ obchod/src/main/java/fim/obchod/ObchodImpl.java
@@ -0,0 +1,5 @@
+package fim.obchod;
+
+public class ObchodImpl {
+    
+}
